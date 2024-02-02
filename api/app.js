8e863db1b1595d2f3dc05f818294ab26e1a2535d
Eddy <ruyushco22@gmail.com>
2024-02-02 02:34:46 -0500

    first commit

---
+++ api/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const PORT = process.env.PORT || 3000; // Puerto 3000 por defecto
+
+// Configuración CORS
+const corsOptions = {
+    origin: '*',  // Reemplaza con la URL de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+    
+app.use(cors(corsOptions));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+module.exports = app;
